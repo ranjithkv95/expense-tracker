91862bf38e9b73e92c0a960b2ea1655e0ee0b008
ranjithkv95 <ba.ranjithvelumani@gmail.com>
2026-02-16 19:10:39 +0530

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -14,13 +14,13 @@ import {
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8voMzOfUNYjDriiNrHZe9EzzHsgn53tc",
+  apiKey: "AIzaSyBX5L0DV_O-Db0-sUPvw80I-i1rtfRX4NA",
   authDomain: "budget-planner-c4791.firebaseapp.com",
   projectId: "budget-planner-c4791",
   storageBucket: "budget-planner-c4791.firebasestorage.app",
   messagingSenderId: "152223224508",
-  appId: "1:152223224508:web:eefd86825cebca821ff6b9",
-  measurementId: "G-JGF1N5JZWR"
+  appId: "1:152223224508:web:5c75fa5eb2b2f3401ff6b9",
+  measurementId: "G-LZQMYLK0J1"
 };
 
 const app = initializeApp(firebaseConfig);
